5de394bb22d731f80b8ddb888c47cabefaa09402
OrtegaGar117 <iaog_250998@hotmail.com>
2025-11-12 17:32:11 -0600

    Se realizaron los ejercicios de JavaScript

--- ejercicio08.js
+++ ejercicio08.js
@@ -7,6 +7,10 @@
 
 //escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
 
+function concatenarCadenas(cadena1, cadena2) {
+    return cadena1 + cadena2;
+}
+
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     concatenarCadenas
